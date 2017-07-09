f61cec08b9ac6cd8f0d56200ffb0c3735d5314f5
Brodie Slater <brodster22@live.com>
2017-07-09 11:58:30 +0100

    write css as sass
    add gulp-clean-css dependency
    
    
    write gulp sass task
    
    
    point link to build/style.min.css
    
    
    ignore build dir + change color vars

--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,39 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
+var minify = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
+    // Return async stream as dependency for 'clean-css'
+    var stream = gulp.src('scss/style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(minify())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./build'));
+    return stream;
 });
 
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
+// gulp.task('scripts', function() {
+//     return gulp.src('js/*.js')
+//         .pipe(concat('all.js'))
+//         .pipe(gulp.dest('dist'))
+//         .pipe(rename('all.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('dist/js'));
+// });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
+    // gulp.watch('js/*.js', ['lint', 'scripts']);
     gulp.watch('scss/*.scss', ['sass']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['sass']);
